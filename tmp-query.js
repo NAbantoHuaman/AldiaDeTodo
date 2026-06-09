ef282e7020f3812844627165e8c857946705c629
NAbantoHuaman <abantohuamanp3@gmail.com>
2026-06-09 16:24:34 -0500

    Actualización

--- tmp-query.js
+++
@@ -1,8 +0,0 @@
-const { PrismaClient } = require('./node_modules/@prisma/client');
-const p = new PrismaClient();
-
-(async () => {
-  const cats = await p.category.findMany();
-  cats.forEach(c => console.log('ID:', c.id, '| Nombre:', c.name, '| Color:', c.color));
-  await p.$disconnect();
-})();
